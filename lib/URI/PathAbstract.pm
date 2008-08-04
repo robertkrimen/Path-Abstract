d41c921e149e50cc5258a5616feaed9fd1ac510f
robertkrimen <robertkrimen@gmail.com>
2008-08-04 10:44:58 -0700

    Removed Path::Abstract::URI (made U::PA the real deal) Check for URI presence when testing Some documentation tweaks

--- lib/URI/PathAbstract.pm
+++ lib/URI/PathAbstract.pm
@@ -3,8 +3,6 @@ package URI::PathAbstract;
 use strict;
 use warnings;
 
-use base qw/Path::Abstract::URI/;
-
 =head1 NAME
 
 URI::PathAbstract - A URI-like object with Path::Abstract capabilities
@@ -22,10 +20,16 @@ URI::PathAbstract - A URI-like object with Path::Abstract capabilities
     $uri->path("grape/blueberry/pineapple")
     # http://example.com/grape/blueberry/pineapple?c=d&e=f
 
+    $uri = $uri->parent
+    # http://example.com/grape/blueberry?c=d&e=f
+
+    $uri = $uri->child("xyzzy")
+    # http://example.com/grape/blueberry/xyzzy?c=d&e=f
+
 =head1 DESCRIPTION
 
 URI::PathAbstract is a combination of the L<URI> and L<Path::Abstract> classes. It is essentially a URI
-class that delegate path-handling methods to Path::Abstract
+class that delegates path-handling methods to Path::Abstract
 
 Unfortunately, this is not true:
 
@@ -34,6 +38,17 @@ Unfortunately, this is not true:
 URI::PathAbstract supports the L<URI> generic and common methods
 
 =cut
+
+use URI;
+use Path::Abstract;
+use Scalar::Util qw/blessed/;
+use Carp;
+
+use overload
+    '""' => sub { $_[0]->{uri}->as_string },
+    '==' => sub { overload::StrVal($_[0]) eq overload::StrVal($_[1]) },
+    fallback => 1,
+;
 
 =head1 METHODS
 
@@ -77,75 +92,244 @@ Returns a URI::PathAbstract that is an exact clone of $uri
 
 =cut
 
-=head2 * URI *
+sub new {
+    my $self = bless {}, shift;
+
+    if (@_ == 1 ) {
+        $self->uri(shift);
+    }
+    elsif (@_ % 2) {
+        $self->uri(shift);
+        my %given = @_;
+        $self->path($given{path}) if exists $given{path};
+        $self->down($given{child}) if exists $given{child};
+    }
+    elsif (@_) {
+        my %given = @_;
+        $self->uri($given{uri});
+        $self->path($given{path}) if exists $given{path};
+        $self->down($given{child}) if exists $given{child};
+    }
+    else {
+        $self->uri(URI->new);
+    }
+
+    return $self;
+}
+
+sub uri {
+    my $self = shift;
+    return $self->{uri}->clone unless @_;
+    my $uri = shift;
+    $uri = URI->new($uri) unless blessed $uri;
+    $self->path($uri->path, 1);
+    $self->{uri} = $uri->clone;
+    return $self;
+}
+
+sub path {
+    my $self = shift;
+    return $self->{path}->clone unless @_;
+    my $path = shift;
+    $path = "" unless defined $path;
+    $path = Path::Abstract->new("$path");
+    my $skip_modifying = shift;
+    $path->to_tree;
+    $self->{path} = $path;
+    $self->{uri}->path($path->get) unless $skip_modifying;
+    return $self;
+}
+
+sub clone {
+    my $self = shift;
+    my $class = ref $self;
+    return $class->new($self->uri);
+}
+
+{
+
+=head2 * L<URI> *
 
 See L<URI> for more information
 
-=head2 $uri->scheme
+=head2 scheme
 
-=head2 $uri->fragment
+=head2 fragment
 
-=head2 $uri->as_string
+=head2 as_string
 
-=head2 $uri->canonical
+=head2 canonical
 
-=head2 $uri->eq
+=head2 eq
 
-=head2 $uri->authority
+=head2 authority
 
-=head2 $uri->query
+=head2 query
 
-=head2 $uri->query_form
+=head2 query_form
 
-=head2 $uri->query_keywords
+=head2 query_keywords
 
-=head2 $uri->userinfo
+=head2 userinfo
 
-=head2 $uri->host
+=head2 host
 
-=head2 $uri->port
+=head2 port
 
-=head2 $uri->host_port
+=head2 host_port
 
-=head2 $uri->default_port
+=head2 default_port
 
-=head2 $uri->abs
+=cut
+
+    no strict 'refs';
+
+    for my $method (grep { ! /^\s*#/ } split m/\n/, <<_END_) {
+scheme
+fragment
+as_string
+canonical
+eq
+authority
+query
+query_form
+query_keywords
+userinfo
+host
+port
+host_port
+default_port
+_END_
+        *$method = sub {
+            my $self = shift;
+            return $self->{uri}->$method(@_);
+        }
+    }
+
+=head2 abs
 
 Returns a L<URI::PathAbstract> object
 
-=head2 $uri->rel
+=head2 rel
 
 Returns a L<URI::PathAbstract> object
 
-=head2 $uri->opaque
+=cut
 
-=head2 $uri->path_query
+    for my $method (grep { ! /^\s*#/ } split m/\n/, <<_END_) {
+abs
+rel
+_END_
+        *$method = sub {
+            my $self = shift;
+            my $uri = $self->{uri}->$method(@_);
+            my $class = ref $self;
+            return $class->new($uri);
+        }
+    }
 
-=head2 $uri->path_segments
+=head2 opaque
 
-=head2 * Path::Abstract *
+=head2 path_query
+
+=head2 path_segments
+
+=head2 * L<Path::Abstract> *
 
 See L<Path::Abstract> for more information
 
-=head2 $uri->child
+=head2 child
 
-=head2 $uri->parent
+=head2 parent
 
-=head2 $uri->list
+=cut
 
-=head2 $uri->first
+    for my $method (grep { ! /^\s*#/ } split m/\n/, <<_END_) {
+child
+parent
+_END_
+        *$method = sub {
+            my $self = shift;
+            my $path = $self->{path}->$method(@_);
+            my $clone = $self->clone;
+            $clone->path($path);
+            return $clone;
+        }
+    }
 
-=head2 $uri->last
+=head2 up
 
-=head2 $uri->is_empty
+=head2 pop
 
-=head2 $uri->is_nil
+=head2 down
 
-=head2 $uri->is_root
+=head2 push
 
-=head2 $uri->is_tree
+=head2 to_tree
 
-=head2 $uri->is_branch
+=head2 to_branch
+
+=cut
+
+    for my $method (grep { ! /^\s*#/ } split m/\n/, <<_END_) {
+up
+pop
+down
+push
+to_tree
+to_branch
+#set
+_END_
+        *$method = sub {
+            my $self = shift;
+            my $path = $self->{path};
+            my @result;
+            if (wantarray) {
+                my @result = $path->$method(@_);
+            }
+            else {
+                $result[0] = $path->$method(@_);
+            }
+            $self->path($$path);
+            return wantarray ? @result : $result[0];
+        }
+    }
+    
+=head2 list
+
+=head2 first
+
+=head2 last
+
+=head2 is_empty
+
+=head2 is_nil
+
+=head2 is_root
+
+=head2 is_tree
+
+=head2 is_branch
+
+=cut
+
+    for my $method (grep { ! /^\s*#/ } split m/\n/, <<_END_) {
+#get
+list
+first
+last
+is_empty
+is_nil
+is_root
+is_tree
+is_branch
+_END_
+        *$method = sub {
+            my $self = shift;
+            return $self->{path}->$method(@_);
+        }
+    }
+}
 
 =head1 SEE ALSO
 
--- t/03-uri.t
+++ t/03-uri.t
@@ -5,50 +5,11 @@ use warnings;
 
 use Test::Most;
 
-use Path::Abstract::URI;
 use URI::PathAbstract;
 
+plan skip_all => "URI is required for testing (and using) URI::PathAbstract" unless eval "require URI;";
+
 plan qw/no_plan/;
-
-{
-    my $uri = Path::Abstract::URI->new("http://example.com?a=b");
-    is($uri, "http://example.com?a=b");
-
-    $uri->down(qw/apple/);
-    is($uri, "http://example.com/apple?a=b");
-
-    $uri->up;
-    $uri->up;
-    $uri->up;
-    is($uri, "http://example.com/?a=b");
-
-    $uri->down(qw/apple banana/);
-    is($uri, "http://example.com/apple/banana?a=b");
-
-    is($uri->parent, "http://example.com/apple?a=b");
-
-    $uri = $uri->parent;
-    $uri->down("");
-    is($uri, "http://example.com/apple?a=b");
-
-    $uri->down("/");
-    is($uri, "http://example.com/apple/?a=b");
-
-    $uri->query("c=d&e=f");
-    is($uri, "http://example.com/apple/?c=d&e=f");
-
-    $uri->path("grape/blueberry/pineapple");
-    is($uri, "http://example.com/grape/blueberry/pineapple?c=d&e=f");
-
-    $uri = Path::Abstract::URI->new("http://example.com/cherry?a=b", path => "grape/lemon");
-    is($uri, "http://example.com/grape/lemon?a=b");
-
-    $uri = Path::Abstract::URI->new("http://example.com/cherry?a=b", child => "grape/lemon");
-    is($uri, "http://example.com/cherry/grape/lemon?a=b");
-
-    $uri = Path::Abstract::URI->new(uri => "http://example.com/cherry?a=b", child => "grape/lemon");
-    is($uri, "http://example.com/cherry/grape/lemon?a=b");
-}
 
 {
     my $uri = URI::PathAbstract->new("http://example.com?a=b");
@@ -79,6 +40,12 @@ plan qw/no_plan/;
 
     $uri->path("grape/blueberry/pineapple");
     is($uri, "http://example.com/grape/blueberry/pineapple?c=d&e=f");
+
+    $uri = $uri->parent;
+    is($uri, "http://example.com/grape/blueberry?c=d&e=f");
+
+    $uri = $uri->child("xyzzy");
+    is($uri, "http://example.com/grape/blueberry/xyzzy?c=d&e=f");
 
     $uri = URI::PathAbstract->new("http://example.com/cherry?a=b", path => "grape/lemon");
     is($uri, "http://example.com/grape/lemon?a=b");
